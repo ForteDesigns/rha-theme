e256286864e41c7412830fdf0fc72c8d7baa8fce
Kyle Ladd <kyleladd7@gmail.com>
2012-12-19 14:48:46 -0500

    Navigation tweaks and search

--- header.php
+++ header.php
@@ -11,27 +11,35 @@
 			<header id="masthead">
 				<div class="top">
 					<div class="wrap">
-						<table>
-							<tr>
-								<td class="msu">
-									<a href="http://msu.edu/">
-									<img src="<?php echo(get_template_directory_uri()."/img/logo.png");?>"/>
-									</a>
-								</td>
-								<td class="rha">
-									<a href="<?php bloginfo('url');?>">
-									<?php bloginfo('description'); ?>
-									</a>
-								</td>
-							</tr>
-						</table>
+						<div id="branding">
+							<table>
+								<tr>
+									<td class="msu">
+										<a href="http://msu.edu/">
+										<img src="<?php echo(get_template_directory_uri()."/img/logo.png");?>"/>
+										</a>
+									</td>
+									<td class="rha">
+										<a href="<?php bloginfo('url');?>">
+										<?php bloginfo('description'); ?>
+										</a>
+									</td>
+								</tr>
+							</table>
+						</div>
+						<form role="search" method="get" action="<?php bloginfo('home');?>">
+							<div id="search">
+								<input type="text" value="<?php echo wp_specialchars($s, 1);?>" name="s" id="s" autocomplete="off"/>
+								<input type="submit" id="searchsubmit" value>
+							</div>
+						</from>
 					</div>
 				</div>
 				<div class="shadow"></div>
 				<div class="bottom">
 					<div class="wrap">
 						<nav>
-							<?php wp_nav_menu(array('show_home'=>TRUE));?> 
+							<?php wp_nav_menu(array('show_home'=>TRUE));?>
 						</nav>
 					</div>
 				</div>
