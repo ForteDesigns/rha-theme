83c938a1ad01d4cf79e0b49b924f0243b9e7c0b7
Kyle Ladd <kyleladd7@gmail.com>
2013-01-07 22:24:29 -0500

    Add sidebar

---
+++ sidebar.php
@@ -0,0 +1,5 @@
+<style>
+	.timeline-header {display:none;}
+</style>
+<a class="twitter-timeline" data-dnt=true href="https://twitter.com/MSURHA" data-widget-id="288487699906707458">Tweets by @MSURHA</a>
+<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
