5d5217f28f6626517ee5227520632b9158059582
Kyle Ladd <kyleladd7@gmail.com>
2013-01-15 02:17:28 -0500

    Add Google Analytics

--- header.php
+++ header.php
@@ -5,6 +5,17 @@
 		<title>
 			<?php bloginfo('name');?> | <?php bloginfo('description');?>
 		</title>
+		<script type="text/javascript">
+			var _gaq = _gaq || [];
+			_gaq.push(['_setAccount', 'UA-37617270-1']);
+			_gaq.push(['_trackPageview']);
+
+			(function() {
+			var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+			ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+			var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+			})();
+		</script>
 		<link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url');?>" />
 	</head>
 	<body>
