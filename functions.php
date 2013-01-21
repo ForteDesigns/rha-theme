751005fc8d0b7a8cddfeaab1e4f06796af46389e
Kyle Ladd <kyleladd7@gmail.com>
2013-01-20 21:34:27 -0500

    Fix read more and get better excerpt tag stripping

--- functions.php
+++ functions.php
@@ -1,13 +1,11 @@
 <?php
 
-/**
- * Control the 'read more' message on front page post
- * summaries
- */
- function new_excerpt_more( $more ) {
- 	return '...';
- }
- add_filter('excerpt_more', 'new_excerpt_more');
+// Puts link in excerpts more tag
+function new_excerpt_more($more) {
+       global $post;
+	return ' | <a class="moretag" href="'. get_permalink($post->ID) . '">Read more...</a>';
+}
+add_filter('excerpt_more', 'new_excerpt_more');
 
 /**
  * Menu control
@@ -20,26 +18,33 @@ register_nav_menus( array(
 /**
  * Show links in excerpt
  */
-function new_trim_excerpt($text) {
-        global $post;
-        if ( '' == $text ) {
-                $text = get_the_content('');
-                $text = apply_filters('the_content', $text);
-                $text = str_replace('\]\]\>', ']]>', $text);
-                $text = preg_replace('@<script[^>]*?>.*?</script>@si', '', $text);
-                $text = strip_tags($text, '<a>');
-                $excerpt_length = 80;
-                $words = explode(' ', $text, $excerpt_length + 1);
-                if (count($words)> $excerpt_length) {
-                        array_pop($words);
-                        array_push($words, '</a><a>ID)' . '">Read the Rest...</a>');
-                        $text = implode(' ', $words);
-                }
-        }
-        return $text;
+function new_wp_trim_excerpt($text) {
+	$raw_excerpt = $text;
+	if ( '' == $text ) {
+		$text = get_the_content('');
+
+		$text = strip_shortcodes( $text );
+
+		$text = apply_filters('the_content', $text);
+		$text = str_replace(']]>', ']]>', $text);
+		$text = strip_tags($text, '<a>');
+		$excerpt_length = apply_filters('excerpt_length', 55);
+
+		$excerpt_more = apply_filters('excerpt_more', ' ' . '[...]');
+		$words = preg_split('/(<a.*?a>)|\n|\r|\t|\s/', $text, $excerpt_length + 1, PREG_SPLIT_NO_EMPTY|PREG_SPLIT_DELIM_CAPTURE );
+		if ( count($words) > $excerpt_length ) {
+			array_pop($words);
+			$text = implode(' ', $words);
+			$text = $text . $excerpt_more;
+		} else {
+			$text = implode(' ', $words);
+		}
+	}
+	return apply_filters('new_wp_trim_excerpt', $text, $raw_excerpt);
+
 }
 remove_filter('get_the_excerpt', 'wp_trim_excerpt');
-add_filter('get_the_excerpt', 'new_trim_excerpt');
+add_filter('get_the_excerpt', 'new_wp_trim_excerpt');
 
 /**
  * Exclude pages from search results
